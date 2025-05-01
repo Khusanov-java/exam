fe199324452af16b7928e63ef7d585046654afb0
Khusanov-babulya <khusanovmirsaid@gmail.com>
2025-05-01 17:47:14 +0500

    user

--- src/main/java/org/example/exam/repository/StatusRepository.java
+++ src/main/java/org/example/exam/repository/StatusRepository.java
@@ -1,7 +1,11 @@
 package org.example.exam.repository;
 
 import org.example.exam.entity.Status;
+import org.example.exam.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StatusRepository extends JpaRepository<Status, Integer> {
+    List<Status> findAllByActiveTrueOrderByPositionNumber();
 }
